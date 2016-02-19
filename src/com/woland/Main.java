8726b3b8a7f35096680afdeb39024126b0612764
temenid <demitriy.s@gmail.com>
2016-02-19 03:20:17 +0200

    initial

--- src/com/woland/Main.java
+++ src/com/woland/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //connection to DB (PostgreSQL)
-
         System.out.println("-------- PostgreSQL "
                 + "JDBC Connection Testing ------------");
 
